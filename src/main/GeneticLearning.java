41290bc4f7a6319d9d0eff15ffe0e6fb3ad568d7
Zach Jones <zachtjones16@gmail.com>
2019-05-02 21:57:07 -0400

    Created multi classifier to apply binary learners

--- src/main/GeneticLearning.java
+++ src/main/GeneticLearning.java
@@ -1,13 +1,8 @@
 package main;
 
-import java.io.IOException;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class GeneticLearning {
-
-	private final static int MAX_POOL_SIZE = 12;
-	private final static int NUMBER_GENERATIONS = 50;
 
 	/** Holds the pool of potential attributes */
 	private final TreeSet<Attributes> pool;
@@ -62,7 +57,7 @@ public class GeneticLearning {
 	 * Proceeds to the next generation, performing mutation, crossover, and then trimming
 	 * the results down to the pool size, keeping the most fit.
 	 */
-	private void nextGeneration() {
+	private void nextGeneration(int maxPoolSize) {
 		// mutate some random ones, proportional to their fitness
 		TreeSet<Attributes> newOnes = new TreeSet<>((o1, o2) -> {
 			double o1D = o1.fitness(inputs, languageOne, languageTwo);
@@ -87,30 +82,29 @@ public class GeneticLearning {
 		pool.addAll(newOnes);
 
 		// trim the pool to MAX_POOL_SIZE (aka natural selection in the genetic sense)
-		while (pool.size() > MAX_POOL_SIZE) {
+		while (pool.size() > maxPoolSize) {
 			pool.pollFirst();
 		}
 	}
 
-
-	public static void main(String[] args) throws IOException {
-		ArrayList<InputRow> inputs = InputRow.loadExamples("training.txt");
-		String languageOne = "English";
-		String languageTwo = "Dutch";
-
-		List<InputRow> englishOrDutch = inputs.stream()
-			.filter(i -> i.outputValue.equals(languageOne) || i.outputValue.equals(languageTwo))
-			.collect(Collectors.toList());
-
+	/***
+	 * Learns a set of attributes from the input data, given a genetic algorithm.
+	 * Precondition: all examples are either language one or two.
+	 * @param inputs The labeled data to learn from (contains all examples, not just languageOne and two).
+	 * @param languageOne The first language.
+	 * @param languageTwo The second language.
+	 * @param numberGenerations The number of generations to run
+	 * @param poolSize The pool size of attributes to retain.
+	 * @return The set of attributes learned.
+	 */
+	static Set<Attributes> learnAttributes(List<InputRow> inputs, String languageOne, String languageTwo,
+											int numberGenerations, int poolSize) {
 		// iterate 100 generations
-		GeneticLearning learning = new GeneticLearning(englishOrDutch, languageOne, languageTwo);
-		for (int i = 0; i < NUMBER_GENERATIONS; i++) {
-			learning.nextGeneration();
+		GeneticLearning learning = new GeneticLearning(inputs, languageOne, languageTwo);
+		for (int i = 0; i < numberGenerations; i++) {
+			learning.nextGeneration(poolSize);
 		}
 
-		// right now just print out the results to see if they're working
-		for (Attributes i : learning.pool) {
-			System.out.println(i + ": " + i.fitness(englishOrDutch, languageOne, languageTwo));
-		}
+		return learning.pool;
 	}
 }
---
+++ src/main/MultiClassifier.java
@@ -0,0 +1,85 @@
+package main;
+
+import helper.Pair;
+import helper.WeightedList;
+
+import java.io.Serializable;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class MultiClassifier implements Serializable, Decider {
+
+	/** Holds a list of all the decision trees to */
+	private List<Decider> allTrees;
+
+
+	private MultiClassifier(List<Decider> allTrees) {
+		this.allTrees = allTrees;
+	}
+
+	/**
+	 * Learns a decision tree with the data provided and the depth.
+	 * First figures out the attributes for each pair of languages, and then learns a decision tree on each.
+	 * @param rows The List of labeled input.
+	 * @param depth The depth of the trees.
+	 * @return A multi-classifier based on simple decision trees.
+	 */
+	static MultiClassifier learnDecisionTree(List<InputRow> rows, int depth) {
+		List<Pair<String, String>> languagePairs = Learning.languagePairs;
+
+		List<Decider> allTrees = new ArrayList<>();
+
+		for (Pair<String, String> pair : languagePairs) {
+
+			String first = pair.one;
+			String second = pair.two;
+
+			System.out.println("\nLearning binary classifier between " + first + " and " + second);
+
+			List<InputRow> twoLanguages = rows.stream()
+				.filter(i -> i.outputValue.equals(first) || i.outputValue.equals(second))
+				.collect(Collectors.toList());
+
+			// TODO make number of generations and pool size a parameter
+			Set<Attributes> attributes =
+				GeneticLearning.learnAttributes(twoLanguages, first, second, 50, 12);
+
+			// learn a decision tree based on the attributes, with depth
+			Decider newTree = DecisionTree.learn(
+				new WeightedList<>(twoLanguages), depth, attributes, twoLanguages.size(), first, second
+			);
+
+			// print out the new tree
+			System.out.println("Learned:\n" + newTree.representation(0));
+
+			// determine accuracy
+			System.out.print("Accuracy of binary classification: ");
+			System.out.println(100 * (1 -newTree.errorRateUnWeighted(twoLanguages)));
+
+			allTrees.add(newTree);
+		}
+
+		return new MultiClassifier(allTrees);
+	}
+
+	@Override
+	public String decide(InputRow row) {
+		// base on the most number of +1's
+		Map<String, Long> counts = allTrees.stream()
+			.map(i -> i.decide(row)) // stream of decisions
+			.collect(Collectors.groupingBy(i -> i, Collectors.counting())); // map< language, count
+
+		// find most common one
+		return counts.entrySet().stream()
+			.max(Comparator.comparingLong(Map.Entry::getValue)) // extract max pair
+			.map(Map.Entry::getKey).orElse(null);
+	}
+
+	@Override
+	public String representation(int numSpaces) {
+		return "Learn using the following deciders:\n" +
+			allTrees.stream()
+				.map(i -> i.representation(numSpaces))
+				.collect(Collectors.joining("\n"));
+	}
+}
