58dd19016bd8a471c89525ea3330ffe1acedccd5
Zach Jones <zachtjones16@gmail.com>
2019-05-02 21:42:49 -0400

    Moved arguments to the main method

--- src/examples/GetWikipediaContent.java
+++ src/examples/GetWikipediaContent.java
@@ -1,14 +1,13 @@
 package examples;
 
 import helper.Pair;
+import main.Learning;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -19,12 +18,6 @@ import java.util.regex.Pattern;
  * This class is used solely to obtain a set of inputs from the several different languages.
  */
 public class GetWikipediaContent {
-
-    /** The output file name for the examples randomly generated. */
-    private static final String outFileName = "testing.txt";
-
-    /** The number of examples of each language to generate */
-    private static final int NUMBER_EXAMPLES_EACH = 200;
 
     /** Regular expression of characters to remove from the input. */
     public static final String REGEX = "[\\-()*&^%$#@!,./?'\";:+«»‘\\[\\]{}=_\\\\|\u8211\u0183°′”″“’ʻ·–—•º„]";
@@ -110,9 +103,9 @@ public class GetWikipediaContent {
     }
 
     /** loads the random articles from Wikipedia, saving to the output file */
-    public static void main(String[] args) throws IOException {
+    public static void main(String outFileName, int numberExamplesEach) throws IOException {
 
-        List<Pair<String, URL>> urls = getLanguageUrls();
+        List<Pair<String, URL>> urls = Learning.getLanguageUrls();
 
         PrintWriter pw = new PrintWriter(outFileName);
 
@@ -121,7 +114,7 @@ public class GetWikipediaContent {
             final URL currentURL = url.two;
 
             int counter = 0;
-            while (counter < NUMBER_EXAMPLES_EACH) {
+            while (counter < numberExamplesEach) {
                 // language|words, separated with spaces
                 pw.print(language);
 
@@ -139,23 +132,4 @@ public class GetWikipediaContent {
         pw.close();
     }
 
-    private static List<Pair<String, URL>> getLanguageUrls() throws MalformedURLException {
-        return Arrays.asList(
-            new Pair<>("Albanian", new URL("https://sq.wikipedia.org/wiki/Speciale:Rast%C3%ABsishme")),
-            new Pair<>("Croatia", new URL("https://hr.wikipedia.org/wiki/Posebno:Slu%C4%8Dajna_stranica")),
-            new Pair<>("Czech", new URL("https://cs.wikipedia.org/wiki/Speci%C3%A1ln%C3%AD:N%C3%A1hodn%C3%A1_str%C3%A1nka")),
-            new Pair<>("Danish", new URL("https://da.wikipedia.org/wiki/Speciel:Tilf%C3%A6ldig_side")),
-            new Pair<>("Dutch", new URL("https://nl.wikipedia.org/wiki/Speciaal:Willekeurig")),
-            new Pair<>("English", new URL("https://en.wikipedia.org/wiki/Special:Random")),
-            new Pair<>("French", new URL("https://fr.wikipedia.org/wiki/Sp%C3%A9cial:Page_au_hasard")),
-            new Pair<>("Gaelic", new URL("https://gd.wikipedia.org/wiki/S%C3%B2nraichte:Random")),
-            new Pair<>("German", new URL("https://de.wikipedia.org/wiki/Spezial:Zuf%C3%A4llige_Seite")),
-            new Pair<>("Hawaiian", new URL("https://haw.wikipedia.org/wiki/Papa_nui:Kaulele")),
-            new Pair<>("Icelandic", new URL("https://is.wikipedia.org/wiki/Kerfiss%C3%AD%C3%B0a:Handah%C3%B3fsvalin_s%C3%AD%C3%B0a")),
-            new Pair<>("Italian", new URL("https://it.wikipedia.org/wiki/Speciale:PaginaCasuale")),
-            new Pair<>("Romanian", new URL("https://ro.wikipedia.org/wiki/Special:Aleatoriu")),
-            new Pair<>("Samoan", new URL("https://sm.wikipedia.org/wiki/Special:Random")),
-            new Pair<>("Spanish", new URL("https://es.wikipedia.org/wiki/Especial:Aleatoria"))
-        );
-    }
 }
