89ca94b996ba793caf5928164314271fb74b3225
Zach Jones <zachtjones16@gmail.com>
2019-05-20 16:10:39 -0400

    Saving current progress, but going to re-architect

--- src/learners/NeuralNetwork.java
+++ src/learners/NeuralNetwork.java
@@ -71,33 +71,148 @@ public class NeuralNetwork implements Decider {
 		// TODO do a loop to continue adjusting weights until they converge
 		// they don't always converge
 		for (int k = 0; k < 30; k++) {
-			// update the final perceptron if incorrect
-			double totalUpdates = 0.0;
-			for (InputRow row : rows) {
-				String decision = this.decide(row).mostConfidentLanguage();
-				if (!decision.equals(row.outputValue)) {
-					// update the perceptron, don't update the intercept
-					for (int i = 1; i < finalPerceptron.weights.length; i++) {
-						// determine the input to finalPerceptron
-						double input = this.attributes.get(i).has(row) ? 1.0 : -1.0;
-						double expectedOutput = row.outputValue.equals(languageOne) ? 1.0 : -1.0;
-						double update = learningRate * input * expectedOutput;
-						totalUpdates += Math.abs(update);
-						finalPerceptron.weights[i] += update;
-					}
-				}
-			}
-			// there's not any changes from the previous iteration, stop.
-			if (totalUpdates == 0) {
+			double result = updateWeights();
+			// stop if the weights don't change
+			if (result == 0.0)
 				break;
-			}
 		}
-		System.out.println();
 
 	}
 
-	@Override
-	public LanguageDecision decide(InputRow row) {
+	/** Updates the weights, returning the total amount of weights changed */
+	private double updateWeights() {
+		// update the final perceptron if incorrect
+		double totalUpdates = 0.0;
+		for (InputRow row : rows) {
+			String decision = this.decide(row).mostConfidentLanguage();
+			if (!decision.equals(row.outputValue)) {
+				totalUpdates = updateWeightsForInput(totalUpdates, row);
+			}
+		}
+
+		return totalUpdates;
+	}
+
+	/** Updates the weights for a specific example.
+	 * This is only called for examples that are classified incorrectly */
+	private double updateWeightsForInput(double totalUpdates, InputRow row) {
+
+		// it's already determined that this classification is incorrect, relay the error back
+		//  throughout the network
+
+		// have to iterate backwards so we don't calculate things twice
+		// responsibility for these goes back from the final node
+
+		// first step: calculate all the activations:
+		// need a working array of doubles for the calculations through, initially the inputs
+		double[][] activations = new double[hiddenLayers][];
+		double[] inputActivations = calculateInputValues(row);
+
+		// working values
+		double[] inputs = inputActivations;
+		double[] nextIteration;
+
+		// work through the network, working left to right, based on the calculations of previous layer
+		for (int i = 0; i < nodes.length; i++) {
+			Perceptron[] currentLayer = nodes[i];
+
+			nextIteration = new double[nodesPerLayer];
+
+			for (int j = 0; j < nodesPerLayer; j++) {
+				double activation = currentLayer[j].calculateResult(inputs);
+				activations[i][j] = activation;
+				nextIteration[j] = activation;
+			}
+
+			// outputs of this layer feed the inputs of the next one.
+			inputs = nextIteration;
+		}
+		final double activationOutput = finalPerceptron.calculateResult(inputs);
+
+		// second step: calculate all the delta's:
+		//  sum over each weight: alpha * (y - predicted)*weight * (1 - predicted)*previous activation
+
+		double deltaOutputNode = 0;
+		double expected = mapOutputToDouble(row.outputValue);
+		// sum over the nodes in the last activations
+		if (hiddenLayers > 0) {
+			// base on last nodes in hidden layer
+			double[] lastActivations = activations[hiddenLayers - 1];
+
+			for (int i = 0; i < nodesPerLayer; i++) {
+				double act = lastActivations[i];
+				double err = learningRate * (expected - activationOutput) * finalPerceptron.weights[i];
+				double slope = (1 - finalPerceptron.weights[i]) * act;
+				deltaOutputNode += err * slope;
+			}
+
+		} else {
+			// base on inputs
+			for (int i = 0; i < inputActivations.length; i++) {
+				double act = inputActivations[i];
+				double err = learningRate * (expected - activationOutput) * finalPerceptron.weights[i];
+				double slope = (1 - finalPerceptron.weights[i]) * act;
+				deltaOutputNode += err * slope;
+			}
+
+		}
+
+		double[][] deltas = new double[hiddenLayers][nodesPerLayer];
+		// calculate back the delta's on the previous ones
+		for (int i = hiddenLayers - 1; i >= 0; i--) {
+			double[] lastActivations = i == 0 ? inputActivations : activations[i - 1];
+			final Perceptron[] goingToPerceptrons;
+			if (i == hiddenLayers - 1) {
+				goingToPerceptrons = new Perceptron[]{finalPerceptron};
+			} else {
+				goingToPerceptrons = nodes[i + 1];
+			}
+
+			for (int j = 0; j < nodesPerLayer; j++) {
+				// calculate the delta for node[i][j] = activation[i][j] * sum of each:
+				//   delta's that this goes to * the weight that goes to it
+				for (Perceptron goingTo : goingToPerceptrons) {
+					final double deltaGoingTo;
+					if (i == hiddenLayers - 1) {
+						deltaGoingTo = deltaOutputNode;
+					} else {
+						deltaGoingTo = deltas[i + 1][j];
+					}
+					deltas[i][j] += deltaGoingTo * goingTo.weights[j];
+				}
+				double slope = (1 - lastActivations[j]);
+				deltas[i][j] *= slope;
+			}
+		}
+
+		// have calculated all delta's, now we can update all the weights
+		for (int i = 0; i < hiddenLayers; i++) {
+			for (int j = 0; j < nodesPerLayer; j++) {
+				// for k up to nodesPerLayer
+				// node[i][j] weight to node[i + 1][k] is incremented by that activation * delta
+				
+			}
+		}
+
+		// update the perceptron, don't update the intercept
+		for (int i = 1; i < finalPerceptron.weights.length; i++) {
+			// determine the input to finalPerceptron
+			double input = this.attributes.get(i).has(row) ? 1.0 : -1.0;
+			double expectedOutput = mapOutputToDouble(row.outputValue);
+			double update = learningRate * input * expectedOutput;
+			totalUpdates += Math.abs(update);
+			finalPerceptron.weights[i] += update;
+		}
+		return totalUpdates;
+	}
+
+	/***
+	 * Calculates the values for the inputs, based on the attributes.
+	 * True is mapped to 1, false is mapped to -1.
+	 * @param row The input to test the attributes on.
+	 * @return A double array representing the activations on the input nodes
+	 */
+	private double[] calculateInputValues(InputRow row) {
 		// true -> 1.0;
 		// false -> -1.0;
 		// calculate the values as they go through the network
@@ -105,9 +220,19 @@ public class NeuralNetwork implements Decider {
 		for (int k = 0; k < numberAttributes; k++) {
 			inputsConverted[k] = attributes.get(k).has(row) ? 1.0 : -1.0;
 		}
+		return inputsConverted;
+	}
 
-		// need a working array of doubles for the calculations through
-		double[] inputs = inputsConverted;
+	/** Maps the language to either 1.0 (first language) or -1.0 (second language) */
+	private double mapOutputToDouble(String language) {
+		return language.equals(languageOne) ? 1.0 : -1.0;
+	}
+
+	@Override
+	public LanguageDecision decide(InputRow row) {
+
+		// need a working array of doubles for the calculations through, initially the inputs
+		double[] inputs = calculateInputValues(row);
 		double[] nextIteration;
 
 		// work through the network, working left to right, based on the calculations of previous layer
